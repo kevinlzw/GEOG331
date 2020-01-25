80fa05c8d401ba9e8dbe37cd6b8ce3822ab2a052
Zhongwen Lian <klian@colgate.edu>
2020-01-25 11:42:22 -0500

    Completed Activity 1

--- activity1/activity1_script.r
+++ activity1/activity1_script.r
@@ -2,4 +2,13 @@
 print("hello R world")
 
 # Calculate a
-a <- 4+4=8
+a <- 4+4
+
+# Create a vector
+vector <- c(5,10,-5,-200)
+
+# Multiply this vector by 2
+vector <- vector * 2
+
+# Calculate 3 to the power of 3
+3^3
